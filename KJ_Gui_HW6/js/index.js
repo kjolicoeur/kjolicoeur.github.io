b8d02ba0459eec658426ff1513813d97f129e2ef
kjolicoe <kyle_jolicoeur@student.uml.edu>
2020-08-06 01:00:20 -0400

    hw6

--- KJ_Gui_HW6/js/index.js
+++ KJ_Gui_HW6/js/index.js
@@ -3,20 +3,92 @@ let form = document.getElementById("form");
 if(form) {
     form.addEventListener('submit',updateTable);
 }
+$.validator.addMethod("greater", function(value, element, params) {
+    console.log("val: "+ value + ", param:" + params.value)
+    if (element.optional) {
+        return false;
+    }
+    else if (params.value == "") {
+        return true;
+    }
+    else if (value >= params.value) {
+            return false;
+    }
+    else {
+        return true;
+    }
+});
+//event listener to validate form
+$("button").click(function() {
+    if($("#form").valid()) {
+        updateTable();
+    }
+});
 //create table
 var table = document.createElement("TABLE");
 table.setAttribute("id","table");
-//strings for error message
-const msg1 = "Your values are invalid";
-const msg2 = " is not a valid number. Use only 0-9.";
+$("#form").validate({
+    rules: {
+        horizStart: {
+            required: true,
+            digits: true,
 
+            greater: horizEnd
+        },
+        horizEnd: {
+            required: true,
+            digits: true,
+        },
+        vertStart: {
+            required: true,
+            digits: true,
+            greater: vertEnd
+        },
+        vertEnd: {
+            required: true,
+            digits: true
+        }
+    },
+    messages: {
+        horizStart: {
+            required: "Please insert a number to the horizontal start#",
+            digits: "Please insert only numbers to the horizontal start#",
+            greater: "Bounds are in wrong order"
+        },
+        horizEnd: {
+            required: "Please insert a number to the horizontal end#",
+            digits: "Please insert only numbers to the horizontal end#",
+            greater: "Bounds are in wrong order"
+        },
+        vertStart: {
+            required: "Please insert a number to the horizontal start#",
+            digits: "Please insert only numbers to the horizontal start#",
+            greater: "Bounds are in wrong order"
+        },
+        vertEnd: {
+            required: "Please insert a number to the horizontal end#",
+            digits: "Please insert only numbers to the horizontal end#",
+            greater: "Bounds are in wrong order"
+        }
+    },
+    errorPlacement: function(error, element) {
+        if (element.attr("name") == "horizStart") {
+            error.appendTo("#errorHoriz");
+        }
+        else if (element.attr("name") == "horizEnd") {
+            error.appendTo("#errorHoriz");
+        }
+        else if (element.attr("name") == "vertStart") {
+            error.appendTo("#errorVert");
+        }
+        else if (element.attr("name") == "vertEnd") {
+            error.appendTo("#errorVert2");
+        }
+    }
+});
 function updateTable() {
-    //reset the error messages to not display
-    document.getElementById('errorHoriz').innerHTML = "";
-    document.getElementById('errorVert').innerHTML = "";
-    document.getElementById('errorHoriz2').innerHTML = "";
-    document.getElementById('errorVert2').innerHTML = "";
 
+    //insert table into HTML file
     if (!document.body.contains(document.getElementById("table"))) {
         document.body.appendChild(table);
     }
@@ -24,87 +96,52 @@ function updateTable() {
         clearTable();
         document.body.appendChild(table);
     }
-
     //grab text from HTML inputs
     let horizStart = document.getElementById("horizStart").value;
     let horizEnd = document.getElementById("horizEnd").value;
     let vertStart = document.getElementById("vertStart").value;
     let vertEnd =  document.getElementById("vertEnd").value;
 
-    if (isNaN(horizStart) || horizStart == "") {
-        document.getElementById('errorHoriz').innerHTML =  msg1 + " \'"+
-        horizStart + "\' " + msg2;
-        return false;
-    }
-    if (isNaN(horizEnd) || horizEnd == "") {
-        document.getElementById('errorHoriz').innerHTML =  msg1 + " \'"+
-        horizEnd + "\' " + msg2;
-        return false;
-    }
-    if (isNaN(vertStart)|| vertStart == "") {
-            document.getElementById('errorVert').innerHTML =  msg1 + " \'"+
-            vertStart + "\' " + msg2;
-            return false;
-    }
-    if (isNaN(vertEnd) || vertEnd == "") {
-            document.getElementById('errorVert').innerHTML =  msg1 + " \'"+
-            vertEnd + "\' " + msg2;
-            return false;
-    }
-    if (horizStart > horizEnd) {
-        let temp = horizStart;
-        horizStart = horizEnd;
-        horizEnd = temp;
-        document.getElementById('errorHoriz2').innerHTML =  'Kyle assumes you meant for your range to go from ' + horizStart + ' to ' + horizEnd;
-    }
-    if (vertStart > vertEnd) {
-        let temp = vertStart;
-        vertStart = vertEnd;
-        vertEnd = temp
-        document.getElementById('errorVert2').innerHTML =  'Kyle assumes you meant for your range to go from ' + vertStart + ' to ' + vertEnd;
-    }
-    //start building the table
+    //--------------start building the table------------------
     //get the range of vertical and horizontal columns
-let horizRange = [];
-let vertRange = [];
-for (let q = horizStart; q <= horizEnd; q++) {
-    horizRange.push(q);
-}
-for (let r = vertStart; r <= vertEnd; r++) {
-    vertRange.push(r);
-}
-
-    //for loop to build table
-    for (let i = 0; i <= vertRange.length; i++) {
-        var rows = document.createElement("tr");
-        for (let j = 0; j <= horizRange.length; j++) {
-            var cell = document.createElement("td");
-            if (i == 0 && j == 0) {//do nothing
-            }
-            else if (i==0 && j !=0) {
-                var newText = document.createTextNode(horizRange[j-1]);
-                cell.appendChild(newText);
-                cell.style.backgroundColor = "black";
-                cell.style.color = "white";
-                cell.style.textAlign = "right";
-            }
-            else if (i != 0 && j == 0) {
-                var newText = document.createTextNode(vertRange[i-1]);
-                cell.appendChild(newText);
-                cell.style.backgroundColor = "black";
-                cell.style.color = "white";
-                cell.style.textAlign = "right";
-            }
-            else {
-                var newText = document.createTextNode(multiply(horizRange[j-1],vertRange [i-1]));
-                cell.appendChild(newText);
-            }
-
-            rows.appendChild(cell);
-        }
-        table.appendChild(rows);
+    let horizRange = [];
+    let vertRange = [];
+    for (let q = horizStart; q <= horizEnd; q++) {
+        horizRange.push(q);
     }
-    table.style.margin = "20px";
+    for (let r = vertStart; r <= vertEnd; r++) {
+        vertRange.push(r);
+    }
+        //for loop to build table
+        for (let i = 0; i <= vertRange.length; i++) {
+            var rows = document.createElement("TR");
+            for (let j = 0; j <= horizRange.length; j++) {
+                var cell = document.createElement("TD");
+                if (i == 0 && j == 0) {//do nothing
+                }
+                else if (i==0 && j !=0) {
+                    var newText = document.createTextNode(horizRange[j-1]);
+                    cell.appendChild(newText);
+                    cell.style.backgroundColor = "black";
+                    cell.style.color = "white";
+                    cell.style.textAlign = "right";
+                }
+                else if (i != 0 && j == 0) {
+                    var newText = document.createTextNode(vertRange[i-1]);
+                    cell.appendChild(newText);
+                    cell.style.backgroundColor = "black";
+                    cell.style.color = "white";
+                    cell.style.textAlign = "right";
+                }
+                else {
+                    var newText = document.createTextNode(multiply(horizRange[j-1],vertRange [i-1]));
+                    cell.appendChild(newText);
+                }
+                rows.appendChild(cell);
+            }
+            table.appendChild(rows);
+        }
+        table.style.margin = "20px";
 }
 function multiply(n1, n2) {
     return n1*n2;
